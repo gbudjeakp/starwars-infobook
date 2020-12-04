2e8775e0221d9d2b6ef05259181cfa46b902cd54
Akpevwen Gbudje <gbudjeakp@gmail.com>
2020-12-04 14:16:09 -0600

    adding content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import axios from 'axios'
 
 function App () {
-  return (<h1>Testing stuff out</h1>)
+  const url = 'https://swapi.dev/api/'
+  axios.get(url)
+    .then(res => {
+      console.log(res.data)
+    })
+
+  return (
+    <div>
+      <input placeholder='search' />
+      <div>
+        <h1>Films</h1>
+        <p>Contain information of all the star war films</p>
+      </div>
+
+      <div>
+        <h1>people</h1>
+        <p>Contain information of all the star war Characters</p>
+      </div>
+
+      <div>
+        <h1>planets</h1>
+        <p>Contain information of all the star war planets</p>
+      </div>
+
+    </div>
+
+  )
 }
 
 export default App
